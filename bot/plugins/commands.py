9b8cdcefdbce0d4e1b29da87cf1d915babb4b158
achuvjklj <87943668+achuvjklj@users.noreply.github.com>
2021-07-26 17:26:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @boxofficeadmin_18,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕️ Join To Channel ⭕️', url="https://t.me/moviesnewsboxoffice"
                                 )
                         ]
                     ]
